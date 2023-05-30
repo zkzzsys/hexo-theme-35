0d78d0db5f40a4aeaae5923b396fc7bb9f5c3287
sharkbaby12345 <134094963+sharkbaby12345@users.noreply.github.com>
2023-05-30 22:01:47 +0800

    Update fomal.js

--- source/js/fomal.js
+++ source/js/fomal.js
@@ -71,9 +71,9 @@ function scrollToTop() {
 //get请求
 $.ajax({
   type: 'get',
-  url: 'https://apis.map.qq.com/ws/location/v1/ip',
+  url: 'https://restapi.amap.com/v3/ip',
   data: {
-    key: '',  // 这里要写你的KEY!!!
+    key: 'd3d5e3d2169236029ab059fc505b59c2',  // 这里要写你的KEY!!!
     output: 'jsonp',
   },
   dataType: 'jsonp',
@@ -99,7 +99,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-  let dist = getDistance(113.34499552, 23.15537143, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+  let dist = getDistance(121.535129, 31.212916, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
   let pos = ipLoacation.result.ad_info.nation;
   let ip;
   let posdesc;
